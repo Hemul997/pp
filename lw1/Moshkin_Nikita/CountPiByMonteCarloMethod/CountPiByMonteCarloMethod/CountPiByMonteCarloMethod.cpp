71faeffa496a791ec7cb80e05ff38bbe4bef0cdc
Hemul997 <nikitamoshkin1997@gmail.com>
2018-03-03 14:03:40 +0300

    Сделал рабочую версию программы в функциональном стиле без многопоточности

--- lw1/Moshkin_Nikita/CountPiByMonteCarloMethod/CountPiByMonteCarloMethod/CountPiByMonteCarloMethod.cpp
+++ lw1/Moshkin_Nikita/CountPiByMonteCarloMethod/CountPiByMonteCarloMethod/CountPiByMonteCarloMethod.cpp
@@ -1,11 +1,41 @@
-// CountPiByMonteCarloMethod.cpp: определяет точку входа для консольного приложения.
-//
+#include "stdafx.h" 
 
-#include "stdafx.h"
+#define MAX_INNER_COUNT 1E6
+#define CIRCLE_RADIUS 1
 
+using namespace std;
+
+bool inCircle(double x, double y);
+double countPI(double innerCount);
+double generateRandomNumber();
 
 int main(int argc, char* argv[])
 {
-    return 0;
+	double x, y, PI;
+	int innerCount = 0;
+	srand(unsigned(time(NULL)));
+
+	for (int i = 0; i < (int)MAX_INNER_COUNT; ++i) {
+		x = generateRandomNumber();
+		y = generateRandomNumber();
+		if (inCircle(x, y)) {
+			innerCount++;
+		}
+	}
+	PI = countPI(innerCount);
+	cout << "Number of PI: " << PI << endl;
 }
 
+double generateRandomNumber() {
+	
+	double random = (double)rand() / (RAND_MAX + CIRCLE_RADIUS) * (CIRCLE_RADIUS - 0) + 0;
+	return random;
+}
+
+bool inCircle(double x, double y) {
+	return (pow(x, 2) + pow(y, 2)) < pow(CIRCLE_RADIUS, 2);
+}
+
+double countPI(double innerCount) {
+	return (4 * innerCount) / MAX_INNER_COUNT;
+}
